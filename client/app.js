04b719ca900eaadae1bceb86ad73af4b630acef9
lmt <1571min@naver.com>
2020-05-05 08:07:13 +0900

    update client post to async post

--- client/app.js
+++ client/app.js
@@ -1,68 +1,75 @@
 // eslint-disable-next-line
 const app = {
-  server: "http://localhost:3000/classes/messages",
-  init: function() {
-    get()
-  }
+  server: 'http://localhost:3000/classes/messages',
+  init: function () {
+    get();
+  },
 };
 
 let serverUrl = null;
 
 const get = () => {
-  if(!serverUrl) {
-    serverUrl = app.server
-  }
+  // if (!serverUrl) {
+  // }
+  serverUrl = app.server;
   fetch(serverUrl, {
-    method: 'GET'
-  }).then(res => {
-
-    return res.json()
-  }).then(data => {
-    dataToHtml(data)
+    method: 'GET',
   })
-  serverUrl = null;
-}
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      dataToHtml(data);
+    });
+  // serverUrl = null;
+};
 
-const post = (message) => {
-  fetch(app.server, {
+const post = async function (message) {
+  let result = await fetch(app.server, {
     method: 'POST',
     body: JSON.stringify(message),
     headers: {
-      "Content-Type": "application/json",
-    }
-  }). then(res => {return res.json()})
-  .then(() => {
-    if(message.roomname === 'all') {
-      serverUrl = app.server;
-    } else {
-      serverUrl = app.server+"?roomname="+message.roomname
-    }
-    read.innerHTML = "";
-    get();
-  })
-}
+      'Content-Type': 'application/json',
+    },
+  });
+  let resultJSON = result.json();
+  // .then((res) => {
+  //   return res.json();
+  // })
+  // .then((res) => {
+  //   // if(message.roomname === 'all') {
+  //   //   serverUrl = app.server;
+  //   // } else {
+  //   //   serverUrl = app.server+"?roomname="+message.roomname
+  //   // }
+  //   read.innerHTML = '';
+  //   console.log(res);
+  //   return res;
+  // });
+  await get();
+  return resultJSON;
+};
 
-
-let read = document.querySelector("#read")
+let read = document.querySelector('#read');
 let display = document.querySelector('.newRoomname');
 
 // eslint-disable-next-line no-unused-vars
 const roomFilter = () => {
   let room = document.getElementById('room');
   let roomName = room.options[room.selectedIndex].value;
-  if(roomName === "New Room") {
+  if (roomName === 'New Room') {
     display.classList.add('newRoomnameDisplay');
   } else {
     display.classList.remove('newRoomnameDisplay');
-    if(roomName === "all") {
+    if (roomName === 'all') {
       serverUrl = app.server;
     } else {
-      serverUrl = app.server+"?roomname="+roomName
+      serverUrl = app.server + '?roomname=' + roomName;
     }
-    read.innerHTML = "";
+    read.innerHTML = '';
     get();
   }
-}
+};
 
 // eslint-disable-next-line no-unused-vars
 const onClick = () => {
@@ -70,34 +77,38 @@ const onClick = () => {
   let content = document.querySelector('#content').value;
   let room = document.getElementById('room');
   let roomName = room.options[room.selectedIndex].value;
-  if(roomName === "New Room") {
+  if (roomName === 'New Room') {
     roomName = document.querySelector('.newRoomname').value;
-    display.classList.remove('newRoomnameDisplay')
+    display.classList.remove('newRoomnameDisplay');
     let newOption = document.createElement('option');
     newOption.value = roomName;
     newOption.textContent = roomName;
     newOption.setAttribute('selected', 'selected');
-    room.add(newOption)
+    room.add(newOption);
   }
-  if(name.indexOf('<' || '>') !== -1 || content.indexOf('<' || '>') !== -1 ||  roomName.indexOf('<' || '>') !== -1 ) {
-    alert('내용에 < 또는 >가 포함되어 있습니다')
+  if (
+    name.indexOf('<' || '>') !== -1 ||
+    content.indexOf('<' || '>') !== -1 ||
+    roomName.indexOf('<' || '>') !== -1
+  ) {
+    alert('내용에 < 또는 >가 포함되어 있습니다');
   } else {
     let message = {
       username: name,
       text: content,
-      roomname: roomName
-    }
-    document.querySelector('#name').value = "";
-    document.querySelector('#content').value = "";
-    document.querySelector('.newRoomname').value = "";
+      roomname: roomName,
+    };
+    document.querySelector('#name').value = '';
+    document.querySelector('#content').value = '';
+    document.querySelector('.newRoomname').value = '';
     post(message);
   }
-}
+};
 
-let template = document.querySelector(".template")
+let template = document.querySelector('.template');
 
 const dataToHtml = (data) => {
-  if(data) {
+  if (data) {
     data.map((x) => {
       let username = x.username;
       let text = x.text;
@@ -107,9 +118,9 @@ const dataToHtml = (data) => {
       // nodes[0].textContent = date;
       nodes[1].textContent = username;
       nodes[2].textContent = text;
-      read.prepend(newTemp)
-    })
+      read.prepend(newTemp);
+    });
   }
-}
+};
 
-app.init()
+app.init();
